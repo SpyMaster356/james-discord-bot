0c6c69e51805de96941feb9cbf1571ce982ee394
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-01-17 19:48:05 -0500

    add !notify and !unsub

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const NixCore = require('nix-core');
+const Path = require('path');
 
-const config = require('./config');
+const localConfig = require('./config');
+const RaidService = require('./lib/services/raid-service');
 
-let james = new NixCore(config);
+let james = new NixCore({
+  dataSource: {
+    type: "disk",
+    dataDir: Path.join(__dirname, './data'),
+  },
+  ...localConfig
+});
 
 james.addModule({
   name: "iron-family",
   enabledByDefault: true,
+  defaultData: [
+    { keyword: RaidService.DataKeys.UsersToNotify, data: [] }
+  ],
   services: [
-    require('./lib/services/raid-service'),
+    RaidService,
   ],
   commands: [
     require('./lib/commands/raid'),
     require('./lib/commands/cancel'),
     require('./lib/commands/start'),
     require('./lib/commands/raiders'),
+    require('./lib/commands/notify'),
+    require('./lib/commands/unsub'),
   ],
 });
 
